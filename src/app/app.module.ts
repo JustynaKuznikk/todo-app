fc21f90f6794662c79173f5b47fe713f838b26b5
JustynaKuznikk <justyna.kuznikk@gmail.com>
2020-09-27 22:28:16 +0200

    todo-app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoHeaderComponent } from './components/todo-header/todo-header.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { TodoListItemComponent } from './components/todo-list-item/todo-list-item.component';
+import { FormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoHeaderComponent,
+    TaskListComponent,
+    TodoListItemComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/task-list/task-list.component.ts
@@ -0,0 +1,14 @@
+import{ Component,  Input  } from '@angular/core';
+
+@Component({
+    selector: 'task-list',
+    templateUrl: 'task-list.component.html'
+})
+export class TaskListComponent  {
+@Input() public tasList: string[];
+
+    
+   
+    
+    
+}
